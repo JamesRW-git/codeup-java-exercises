73f717ce71d4702fcbe9ebbdf1051fc713e3d589
James Woodward <jamesrobertwoodward@gmail.com>
2022-04-08 10:26:30 -0500

    revised exercise answer

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,12 +1,10 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String education = "We don't need no education";
-        String thought = "We don't need no thought control";
+        String education = "We don't need no education\n" + "We don't need no thought control";
         String checkOut = "Check \"this\" out!, \"s inside of \"s!";
         String bindows = "In windows, the main drive is usually C:\\";
         String backslashes = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
         System.out.println(education);
-        System.out.println(thought);
         System.out.println(checkOut);
         System.out.println(bindows);
         System.out.println(backslashes);
