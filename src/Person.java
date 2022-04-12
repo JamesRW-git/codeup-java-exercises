8a5669e0a45fe3e4ba564216c3debbc0d14d30ea
James Woodward <jamesrobertwoodward@gmail.com>
2022-04-12 10:33:25 -0500

    changes

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,20 @@
 public class Person {
     public static void main(String[] args) {
-        Person james = new Person();
-        james.setName("James");
+        Person james = new Person("James");
+//        james.setName("James");
         System.out.println(james.getName());
         james.sayHello();
     }
 
     private String name;
+
+    public Person(){
+
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return this.name;
