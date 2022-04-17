0ea3c9f1be4f503c765a9aa8f01a6a68a12c404d
James Woodward <jamesrobertwoodward@gmail.com>
2022-04-17 15:08:24 -0500

    add ShapesTest.java and code for exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,14 +2,13 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5,4);
-        System.out.println(box1);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
+        Measurable myShape = new Square(5);
 
-        Rectangle box2 = new Square(5);
-        System.out.println(box2);
-        System.out.println(box2.getPerimeter());
-        System.out.println(box2.getArea());
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+
+        Measurable myShapeTwo = new Rectangle(10, 5);
+        System.out.println(myShapeTwo.getArea());
+        System.out.println(myShapeTwo.getPerimeter());
     }
 }
