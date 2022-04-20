a49ea7ab891f3579ffa954cf84027e7fd5e4ed1e
James Woodward <jamesrobertwoodward@gmail.com>
2022-04-20 16:03:12 -0500

    update code after follow along

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,15 +6,49 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static grades.Student.getGradeAverage;
 import static util.Input.getString;
 
 public class GradesApplication {
     public static void main(String[] args) {
+        HashMap<String, Student> students = getStudents();
+
+        boolean keepSearching = true;
+        do {
+            initGradesApp(students);
+            String userContinue = getString("Would you like to search again? (y/n)");
+            if (userContinue.equalsIgnoreCase("y")) {
+                keepSearching = true;
+            } else {
+                keepSearching = false;
+            }
+        } while (keepSearching);
+    }
+
+    private static void initGradesApp(HashMap<String, Student> students){
+        System.out.println("Student Github Usernames:");
+        System.out.println("\n");
+        for(String key : students.keySet()) {
+            System.out.println(key);
+        }
+
+        System.out.println("\n");
+        String userQuery = getString("Which student would you like to see more info about?");
+
+        if (students.containsKey(userQuery)) {
+            Student chosenOne = students.get(userQuery);
+            System.out.println("Name: " + chosenOne.getStudentName());
+            System.out.println("GitHub Username: " + userQuery);
+            System.out.println("Grade Average: " + chosenOne.getGradeAverage());
+        } else {
+            System.out.println("The user " + userQuery + " does not exist");
+        }
+    }
+    private static HashMap<String, Student> getStudents() {
         Student student1 = new Student("Kyle");
         student1.addGrade(100);
         student1.addGrade(95);
         student1.addGrade(97);
+
 
         Student student2 = new Student("Rhagen");
         student2.addGrade(90);
@@ -26,6 +60,7 @@ public class GradesApplication {
         student3.addGrade(85);
         student3.addGrade(84);
 
+
         Student student4 = new Student("Penelope");
         student4.addGrade(100);
         student4.addGrade(100);
@@ -33,26 +68,10 @@ public class GradesApplication {
 
         HashMap<String, Student> students = new HashMap<>();
 
-        students.putIfAbsent("JavanaMan", student1);
-        students.putIfAbsent("GittinCode", student2);
-        students.putIfAbsent("Cobold", student3);
-        students.putIfAbsent("ConfusedApe", student4);
-
-
-        System.out.println("Student Github Usernames:");
-        System.out.println("\n");
-        for(String key : students.keySet()) {
-            System.out.println(key);
-        }
-
-        System.out.println("\n");
-        String userQuery = getString("Which student would you like to see more info about?");
-
-        if (students.containsKey(userQuery)) {
-            System.out.println("Name: " + students.get(userQuery).getStudentName());
-            System.out.println("GitHub Username: " + userQuery);
-            System.out.println("Current Average: " + students.get(userQuery).getGradeAverage());
-        }
-
+        students.put("JavanaMan", student1);
+        students.put("GittinCode", student2);
+        students.put("Cobold", student3);
+        students.put("ConfusedApe", student4);
+        return students;
     }
 }
