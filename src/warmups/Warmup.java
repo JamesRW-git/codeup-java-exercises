eabf1dd3845510024f0a3903cc24257017031fd1
James Woodward <jamesrobertwoodward@gmail.com>
2022-04-21 14:10:11 -0500

    add code for morning warmup

--- src/warmups/Warmup.java
+++ src/warmups/Warmup.java
@@ -1,41 +1,72 @@
 package warmups;
 
+import util.Input;
+
 import java.util.Scanner;
 
 public class Warmup {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Do you live in Dallas? (y/n)");
+//
+//        String userLivesInDallas = scanner.next();
+//        boolean livesInCity;
+//
+//        if (userLivesInDallas.equalsIgnoreCase("y")) {
+//            livesInCity = true;
+//        } else {
+//            livesInCity = false;
+//        }
+//
+//        System.out.println("Do you have transportation? (y/n)");
+//
+//        String userHasTransport = scanner.next();
+//        boolean hasTransportation;
+//
+//        if (userHasTransport.equalsIgnoreCase("y")) {
+//            hasTransportation = true;
+//        } else {
+//            hasTransportation = false;
+//        }
+//
+//        boolean canComeInPerson = livesInCity && hasTransportation;
+//
+//        if (canComeInPerson) {
+//            System.out.println("Yay, you can come to the potluck!");
+//        } else {
+//            System.out.println("Sorry, you're out of (pot)luck");
+//        }
 
-        System.out.println("Do you live in Dallas? (y/n)");
+        boolean userContinue = true;
+        do {
+            String checkPalindrome = Input.getString("Enter a string to check if it is a palindrome: ");
+            System.out.println("Is " + checkPalindrome + " a palindrome? It's " + isPalindrome(checkPalindrome));
 
-        String userLivesInDallas = scanner.next();
-        boolean livesInCity;
+            String userChoice = Input.getString("Would you like to check another word? (y/n)");
+            if (userChoice.equalsIgnoreCase("y")) {
+                userContinue = true;
+            } else {
+                userContinue = false;
+            }
+        } while (userContinue);
+    }
 
-        if (userLivesInDallas.equalsIgnoreCase("y")) {
-            livesInCity = true;
-        } else {
-            livesInCity = false;
+    public static boolean isPalindrome(String word) {
+        String reversed = "";
+        char character;
+        boolean palindrome;
+        for (int i = 0; i < word.length(); i++) {
+            character = word.charAt(i);
+            reversed = character + reversed;
         }
-
-        System.out.println("Do you have transportation? (y/n)");
-
-        String userHasTransport = scanner.next();
-        boolean hasTransportation;
-
-        if (userHasTransport.equalsIgnoreCase("y")) {
-            hasTransportation = true;
+        if (word.equalsIgnoreCase(reversed)) {
+            palindrome = true;
         } else {
-            hasTransportation = false;
+            palindrome = false;
         }
-
-        boolean canComeInPerson = livesInCity && hasTransportation;
-
-        if (canComeInPerson) {
-            System.out.println("Yay, you can come to the potluck!");
-        } else {
-            System.out.println("Sorry, you're out of (pot)luck");
-        }
+        return palindrome;
     }
 }
